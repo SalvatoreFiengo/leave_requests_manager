a59980278cc3b5a2ecbfbadcb89882d1332533c9
SalvatoreFiengo <sal.fiengo@gmail.com>
2019-11-09 17:29:00 +0000

    hide mongodb password and testing heroku app

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"]="leave_request_manager"
-app.config["MONGO_URI"]="mongodb+srv://root:xxx@myfirstcluster-rz7za.mongodb.net/leave_request_manager?retryWrites=true&w=majority"
+app.config["MONGO_URI"]=os.getenv("MONGO_URI")
 
 mongo = PyMongo(app)
 
