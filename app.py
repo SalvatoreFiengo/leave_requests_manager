8aa69ec84c209419c2540dcaae782c84b60da96f
SalvatoreFiengo <sal.fiengo@gmail.com>
2019-11-09 18:33:37 +0000

    testing on heroku by removing local env variables

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-import env 
 
 app = Flask(__name__)
 
